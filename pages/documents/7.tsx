12313f99ee395277dfa31068f8637fc95c709d4d
Cosmic-rare <ysolakey1116@gmail.com>
2024-07-15 11:43:49 +0900

    googleのembend使う

--- pages/documents/7.tsx
+++ pages/documents/7.tsx
@@ -1,10 +1,23 @@
 import Head from "next/head"
+import { useState, useEffect } from "react"
 
-const width = {
-  xs: 0.9, sm: 400, md: 500, lg: 500, xl: 500,
+const resize = () => {
+  const height = window.innerHeight
+  const width = window.innerWidth
+  return { height: height, width: width }
 }
 
 const Documents = () => {
+  const [innerSize, setInnerSize] = useState({ height: 0, width: 0 })
+  const handleResize = () => {
+    setInnerSize(resize())
+  }
+  useEffect(() => {
+    handleResize()
+    window.addEventListener("resize", handleResize)
+    window.addEventListener("orientationchange", handleResize)
+  }, [])
+
   return (
     <div style={{ height: "100%" }}>
       <Head>
@@ -14,11 +27,10 @@ const Documents = () => {
       <div style={{ display: "flex", justifyContent: "center", height: "100%", flexDirection: "column" }}>
         <h2>要項</h2>
 
-<div style={{ height: "100%" }}>
-<embed src="https://s3.tani-exe.net/tournament2024/R6スポーツ大会晴晴B日程.pdf" width="100%" style={{ height: "calc(var(--100vh) - 155px)" }}
-          type="application/pdf"></embed>
-</div>
-  
+        <div style={{ height: "100%" }}>
+          <iframe src="https://drive.google.com/file/d/1CVDbysqVmznTZqew_APNSp9prQAMfyyk/preview" width="100%" style={{ height: `${innerSize.height - 160}px` }} allow="autoplay"></iframe>
+        </div>
+
       </div>
     </div>
   )
