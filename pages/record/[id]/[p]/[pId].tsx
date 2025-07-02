f1a44f56578d260eddf48e559951023b73284aa6
Cosmic-rare <tani516158@gmail.com>
2025-07-02 21:17:27 +0900

    APIで保存する前までrecord奸佞

--- pages/record/[id]/[p]/[pId].tsx
+++ pages/record/[id]/[p]/[pId].tsx
@@ -7,6 +7,10 @@ import { ModalContent } from "@/components/record/inputs"
 import { CircularProgress, Backdrop, Button } from "@mui/material"
 import getClass from "@/util/cl"
 import Head from "next/head"
+import draw from "@/util/draw"
+import data1 from "../../../data1.json"
+import _ from "lodash"
+import { Modal } from "antd"
 
 // const Post = () => {
 //   const router = useRouter()
@@ -114,13 +118,113 @@ import Head from "next/head"
 //   }
 // }
 
+import { TournamentCellData } from "@/pages/tournament"
+import { wl } from "@/util/wl"
+
+const Tournament: React.FC<{ cells: Record<string, TournamentCellData>, data: any }> = ({ cells, data }) => {
+  const colors = ["#adb5bd", "#dc3545"]
+  const width = 30
+  const height = 50
+
+  return (
+    <>
+      {Object.entries(cells).map(([cell, cellData]) => {
+        const cellStyle: React.CSSProperties = {
+          position: "absolute",
+          top: `${(5 - parseFloat(cell.split("_")[1])) * height}px`,
+          left: `${parseFloat(cell.split("_")[0]) * width}px`,
+          height: `${height}px`,
+          width: `${width}px`,
+          paddingRight: cellData.align_left ? "10px" : "0",
+          borderTop: cellData.border_top ? `3px solid ${colors[cellData.border_top - 1]}` : "none",
+          borderLeft: cellData.border_left ? `3px solid ${colors[cellData.border_left - 1]}` : "none",
+          verticalAlign: "bottom",
+          display: "flex",
+          alignItems: `${cell.split("_")[1] === "0" || cellData.edit !== undefined ? "" : "flex-end"}`,
+        }
+
+        return (
+          <div key={cell} style={cellStyle}>
+            <div className={cellData.class} style={{ fontSize: "0.8em", width: "100%", textAlign: cellData.align_left ? "left" : "center", color: cellData.color ? colors[cellData.color - 1] : "inherit", verticalAlign: "bottom" }}>
+              {cellData.text}
+
+              {/* @ts-ignore */}
+              {["esport", "soccer"].includes(data.event) ? <span style={{ color: colors[1] }}>{cellData.point >= 0 ? cellData.point : ""}</span> : null}
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+type RoundValue = number | ''
+
 const Post = () => {
   const [phase, setPhase] = useState(0)
+  const [match, setMatch] = useState<any>()
+  const [results, setResults] = useState<any>({
+    upper: Array(3).fill(''),
+    lower: Array(3).fill(''),
+  });
 
-  if (phase == 0) {
+  const router = useRouter()
+  const { pId, id, p } = router.query
+
+  const template = _.cloneDeep(data1)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!(pId && id && p)) return
+      const res = await APIget(`match/${id}`, () => { }, () => { })
+      setMatch(res)
+    }
+    fetchData()
+  }, [pId, id, p])
+
+  useEffect(() => {
+    if (!match) return
+    setMatch((pMatch: any) => {
+      for (let round = 1; round <= 3; round++) {
+        const lKey = `l_p${round}`;
+        const hKey = `h_p${round}`;
+
+        pMatch[`p_${p}`][lKey] = results.upper[round - 1];
+        pMatch[`p_${p}`][hKey] = results.lower[round - 1];
+      }
+      return pMatch
+    })
+  }, [results])
+
+  const handleChange = (
+    group: string,
+    roundIndex: number,
+    value: string
+  ) => {
+    const newValue: RoundValue = value === '' ? '' : parseInt(value, 10);
+
+    // @ts-ignore
+    if (value === '' || !isNaN(newValue)) {
+      // @ts-ignore
+      setResults(prev => ({
+        ...prev,
+        // @ts-ignore
+        [group]: prev[group].map((r, i) =>
+          i === roundIndex ? newValue : r
+        ),
+      }));
+    }
+  };
+
+  if (phase == 0 && match) {
     return (
       <div>
-        <p>どこどこの試合、あと注意文</p>
+        <p>{match.title}</p>
+        <p>{match.sex}</p>
+        <p>{match[`p_${2}`].scheduledAt}</p>
+        {/* @ts-ignore */}
+        <p>{match.match[parseInt(p ? p : "1") - 1][0]}たい{match.match[parseInt(p ? p : "1") - 1][1]}</p>
+        <p>注意文</p>
         <button
           onClick={() => setPhase(1)}
         >
@@ -134,6 +238,40 @@ const Post = () => {
     return (
       <div>
         <p>入力画面</p>
+        <table style={{ borderCollapse: 'collapse', width: '100%', maxWidth: '400px' }}>
+          <thead>
+            <tr>
+              <th style={{ border: '1px solid #ccc', padding: '6px' }}>ラウンド</th>
+              <th style={{ border: '1px solid #ccc', padding: '6px' }}>upper</th>
+              <th style={{ border: '1px solid #ccc', padding: '6px' }}>lower</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 3 }, (_, i) => (
+              <tr key={i}>
+                <td style={{ border: '1px solid #ccc', textAlign: 'center' }}>{i + 1}</td>
+                <td style={{ border: '1px solid #ccc', padding: '6px' }}>
+                  <input
+                    type="number"
+                    value={results.upper[i]}
+                    onChange={(e) => handleChange('upper', i, e.target.value)}
+                    style={{ width: '100%' }}
+                  />
+                </td>
+                <td style={{ border: '1px solid #ccc', padding: '6px' }}>
+                  <input
+                    type="number"
+                    value={results.lower[i]}
+                    onChange={(e) => handleChange('lower', i, e.target.value)}
+                    style={{ width: '100%' }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <pre>{JSON.stringify(results, null, 2)}</pre>
+        <code>{JSON.stringify(match, null, 2)}</code>
         <button
           onClick={() => setPhase(2)}
         >
@@ -147,6 +285,38 @@ const Post = () => {
     return (
       <div>
         <p>確認画面、トーナメント表がこうなるよーってやつとか時間あったらいれたい</p>
+        <div style={{ width: `${30 * 15 + 10}px`, height: `${320 + 10}px`, overflowY: "hidden", position: "relative" }}>
+          <Tournament
+            cells={draw(match, template, match.event, true)}
+            data={match}
+          />
+        </div>
+        {/* @ts-ignore */}
+        <p>{match.match[parseInt(p ? p : "1") - 1][wl(match[`p_${p}`], match.event, false, true) == 'l' ? 0 : 1]}</p>
+
+        <table style={{ borderCollapse: 'collapse', width: '100%', maxWidth: '400px' }}>
+          <thead>
+            <tr>
+              <th style={{ border: '1px solid #ccc', padding: '6px' }}>ラウンド</th>
+              <th style={{ border: '1px solid #ccc', padding: '6px' }}>l</th>
+              <th style={{ border: '1px solid #ccc', padding: '6px' }}>h</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[1, 2, 3].map(round => {
+              const l = match[`p_${p}`][`l_p${round}`]
+              const h = match[`p_${p}`][`h_p${round}`]
+              return (
+                <tr key={round}>
+                  <td style={{ border: '1px solid #ccc', textAlign: 'center' }}>{round}</td>
+                  <td style={{ border: '1px solid #ccc', textAlign: 'center' }}>{l}</td>
+                  <td style={{ border: '1px solid #ccc', textAlign: 'center' }}>{h}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
         <button
           onClick={() => setPhase(3)}
         >
@@ -159,7 +329,7 @@ const Post = () => {
   if (phase == 3) {
     return (
       <div>
-        <p>完了とだけ、あとページ閉じてねメッセージ</p>
+        <p>完了とだけ、あとページ閉じてねメッセージ、あ、紙を会室にもってきてってのも載せる</p>
       </div>
     )
   }
