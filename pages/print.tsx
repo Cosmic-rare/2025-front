6fd0c359c0b784eba53c89b3ea7cbef6ff9ce0d2
Cosmic-rare <tani516158@gmail.com>
2025-07-05 14:52:18 +0900

    A4のpdfを生成する用のページ

--- pages/print.tsx
+++ pages/print.tsx
@@ -1,20 +1,19 @@
 import { ReactNode, useState, useEffect } from "react"
 
-const width = {
-  xs: 0.9, sm: 350, md: 450, lg: 450, xl: 450,
-}
-
 const resize = () => {
   const width = window.innerWidth
-  const height = width * 1.45
-  return { height: height, width: width }
+  const height = Math.round(width * 1.455)
+  const height25 = height / 4
+  const height50 = height - 2 * height25
+  return { height: height, width: width, height25: height25, height50: height50 }
 }
 
 const Print = () => {
-  const [innerSize, setInnerSize] = useState({ height: 0, width: 0 })
+  const [innerSize, setInnerSize] = useState({ height: 0, width: 0, height25: 0, height50: 0 })
   const handleResize = () => {
     setInnerSize(resize())
   }
+
   useEffect(() => {
     handleResize()
     window.addEventListener("resize", handleResize)
@@ -24,15 +23,12 @@ const Print = () => {
 
   return (
     <div>
-      <div style={{ width: innerSize.width, height: innerSize.height, backgroundColor: 'red' }}>
-        afsdasdfasdf
-      </div>
-      <div style={{ width: innerSize.width, height: innerSize.height, backgroundColor: 'blue' }}>
-        bbbbbbbbb
-      </div>
-      <div style={{ width: innerSize.width, height: innerSize.height, backgroundColor: 'green' }}>
-        bbbbbbbbb
-      </div>
+      <div style={{ width: innerSize.width, height: innerSize.height25, backgroundColor: 'green' }}>名前、注意文、QRコード</div>
+      <div style={{ width: innerSize.width, height: innerSize.height25, backgroundColor: 'red' }}>何も書かない</div>
+      <div style={{ width: innerSize.width, height: innerSize.height50, backgroundColor: 'blue' }}>役員以外開封禁止って書いてあるホチキスの場所が上、左にトーナメント記録者名種目指示など、右に記録する表とかフラグとか</div>
+      <div style={{ width: innerSize.width, height: innerSize.height25, backgroundColor: 'orange' }}></div>
+      <div style={{ width: innerSize.width, height: innerSize.height25, backgroundColor: 'gray' }}></div>
+      <div style={{ width: innerSize.width, height: innerSize.height50, backgroundColor: 'black' }}></div>
     </div>
   )
 
